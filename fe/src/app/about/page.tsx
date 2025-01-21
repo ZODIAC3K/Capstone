fc3ed2595cf2f7047acd10c38f0ef1a261e78804
Harsh Deepanshu <harsh.deepanshu2021@vitbhopal.ac.in>
2025-01-21 12:06:15 +0530

    fix: added new images and changed the ui to better represent our product

--- fe/src/app/about/page.tsx
+++ fe/src/app/about/page.tsx
@@ -7,35 +7,38 @@ import ServicesArea from "../components/services/services-area";
 import TeamArea from "../components/team/team-area";
 
 export default function AboutPage() {
-  return (
-    <Wrapper>
-      {/* header start */}
-      <Header />
-      {/* header end */}
+	return (
+		<Wrapper>
+			{/* header start */}
+			<Header />
+			{/* header end */}
 
-      {/* main area start */}
-      <main className="main--area">
-        {/* breadcrumb area start */}
-        <BreadcrumbArea title="ABOUT US" subtitle="ABOUT US" />
-        {/* breadcrumb area end */}
+			{/* main area start */}
+			<main className="main--area">
+				{/* breadcrumb area start */}
+				{/* <BreadcrumbArea
+					title="ABOUT US"
+					subtitle="ABOUT US"
+				/> */}
+				{/* breadcrumb area end */}
 
-        {/* about area start */}
-        <AboutAreaThree/>
-        {/* about area end */}
+				{/* about area start */}
+				<AboutAreaThree />
+				{/* about area end */}
 
-        {/* Services area start*/}
-        <ServicesArea/>
-        {/* Services area end*/}
+				{/* Services area start*/}
+				<ServicesArea />
+				{/* Services area end*/}
 
-        {/* team area start */}
-        <TeamArea/>
-        {/* team area end */}
-      </main>
-      {/* main area end */}
+				{/* team area start */}
+				{/* <TeamArea/> */}
+				{/* team area end */}
+			</main>
+			{/* main area end */}
 
-      {/* footer start */}
-      <Footer/>
-      {/* footer end */}
-    </Wrapper>
-  );
+			{/* footer start */}
+			<Footer />
+			{/* footer end */}
+		</Wrapper>
+	);
 }
